6f5d278f7b6b0a23230b50e8eb0c72d25e162ca9
Simon Cross <hodgestar@gmail.com>
2014-07-13 12:45:47 +0200

    Clean-up authentication backend interface definition.

--- go_auth/backends/interfaces.py
+++ go_auth/backends/interfaces.py
@@ -1,12 +1,36 @@
-class MemoryAuthBackend(object):
-    """ Simple backed that stores authorization codes and access tokens
-        in memory.
+""" Interfaces for backends.
+"""
+
+from zope.interface import Interface
+
+
+class IAuthBackend(Interface):
     """
-    def __init__(self, tokens):
-        self._tokens = tokens
+    Backend for storing and retrieving authorization codes and
+    access tokens.
+    """
 
-    def get_token(self, client_id):
-        return self._tokens.get(client_id)
+    def get_access_token(client_id):
+        """
+        Retrieve the token associated with the client_id.
 
-    def save_token(self, client_id, token):
-        self._tokens[client_id] = token
+        :param str client_id:
+            The client id to retrieve the access token for.
+
+        :return:
+            The access token (possibly via a deferred) or
+            None if no access token exists.
+        """
+
+    def save_access_token(client_id, token):
+        """
+        Save a token for the associated client_id.
+
+        :param str client_id:
+            The client id the access token belongs to.
+        :param token:
+            The token to store.
+
+        :return:
+            A deferred that fires once the token is stored.
+        """
