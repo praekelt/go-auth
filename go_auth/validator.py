074c67ccec8aa5be0a4635b37a09a5f3525b2c9d
Simon Cross <hodgestar@gmail.com>
2014-07-18 13:24:55 +0200

    First draft of validator.

--- go_auth/validator.py
+++ go_auth/validator.py
@@ -5,12 +5,29 @@ from oauthlib.oauth2 import RequestValidator, WebApplicationServer
 from twisted.internet.defer import inlineCallbacks, returnValue
 
 
-class AuthValidator(RequestValidator):
+class StaticAuthValidator(RequestValidator):
     """ An oauthlib.oauth2 request validator.
+
+    A validator built on top of a dictionary of access tokens.
+
+    :param dict auth_store:
+        The authentication store, a dictionary mapping access
+        tokens to credentials.
+
+    Example auth_store::
+
+       {
+           "ac3sst0k3n": {
+               "client_id": "cl13nt51d",
+               "scopes": ["scope1", "scope2"],
+           },
+       }
     """
 
-    def __init__(self, backend):
-        self._backend = backend
+    def __init__(self, auth_store=None):
+        if auth_store is None:
+            auth_store = {}
+        self.auth_store = auth_store
 
     @inlineCallbacks
     def validate_bearer_token(self, token, scopes, request):
@@ -18,20 +35,53 @@ class AuthValidator(RequestValidator):
             request.client_id)
         if stored_token is None:
             returnValue(False)
-        if stored_token['access_token'] != token['access_token']:
+        if stored_token['access_token'] != token:
             returnValue(False)
         stored_scopes = stored_token['scopes'].split()
-        available_scopes = set(stored_scopes) & set(scopes)
+        available_scopes = set(stored_scopes) & set(scopes or ['foo'])
         # validate as true if any scopes are available
         returnValue(bool(available_scopes))
 
     def save_bearer_token(self, token, request, *args, **kw):
         return self._backend.store_access_token(request.client_id, token)
 
+    def validate_client_id(self, client_id, request, *args, **kwargs):
+        # Simple validity check, does client exist? Not banned?
+        request.client_id = client_id
 
-def web_app_authenticator(backend):
-    """ Return a Vumi Go web app authenticator.
+    def validate_redirect_uri(self, client_id, redirect_uri, request,
+                              *args, **kwargs):
+        # Is the client allowed to use the supplied redirect_uri? i.e. has
+        # the client previously registered this EXACT redirect uri.
+        pass
+
+    def get_default_redirect_uri(self, client_id, request, *args, **kwargs):
+        # The redirect used if none has been supplied.
+        # Prefer your clients to pre register a redirect uri rather than
+        # supplying one on each authorization request.
+        pass
+
+    def validate_scopes(self, client_id, scopes, client, request,
+                        *args, **kwargs):
+        # Is the client allowed to access the requested scopes?
+        pass
+
+    def get_default_scopes(self, client_id, request, *args, **kwargs):
+        # Scopes a client will authorize for if none are supplied in the
+        # authorization request.
+        pass
+
+    def validate_response_type(self, client_id, response_type, client, request,
+                               *args, **kwargs):
+        # Clients should only be allowed to use one type of response type, the
+        # one associated with their one allowed grant type.
+        # In this case it must be "code".
+        pass
+
+
+def static_web_authenticator(auth_store):
+    """ Return a Vumi Go static web authenticator.
     """
-    validator = AuthValidator(backend)
+    validator = StaticAuthValidator(auth_store)
     authenticator = WebApplicationServer(validator)
     return authenticator
