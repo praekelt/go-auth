96a1ec82bb1f16df82c2151d85c758e81fdf6ef7
Simon Cross <hodgestar@gmail.com>
2014-07-18 13:24:43 +0200

    First tests for validator.

--- go_auth/tests/test_validator.py
+++ go_auth/tests/test_validator.py
@@ -3,26 +3,27 @@
 
 from oauthlib.oauth2 import RequestValidator, WebApplicationServer
 from oauthlib.common import Request
-from twisted.internet.defer import inlineCallbacks
 from twisted.trial.unittest import TestCase
 
-from go_auth.backends.inmemory import InMemoryAuthBackend
-from go_auth.validator import AuthValidator, web_app_authenticator
+from go_auth.validator import (
+    StaticAuthValidator, static_web_authenticator)
 
 
-class TestValidatorMisc(TestCase):
-    def test_web_app_authenticator(self):
-        auth = InMemoryAuthBackend()
-        authenticator = web_app_authenticator(auth)
-        self.assertTrue(isinstance(authenticator, WebApplicationServer))
+class TestStaticWebAuthenticator(TestCase):
+    def test_create(self):
+        auth_store = {}
+        auth = static_web_authenticator(auth_store)
+        self.assertTrue(isinstance(auth, WebApplicationServer))
         self.assertTrue(
-            isinstance(authenticator.request_validator, AuthValidator))
-        self.assertEqual(authenticator.request_validator._backend, auth)
+            isinstance(auth.request_validator, StaticAuthValidator))
+        self.assertEqual(auth.request_validator.auth_store, auth_store)
 
 
 class TestAuthValidator(TestCase):
     def setUp(self):
-        self.auth = InMemoryAuthBackend()
+        self.auth_store = {}
+        self.auth = static_web_authenticator(self.auth_store)
+        self.validator = self.auth.request_validator
 
     def mk_token(self, access_token=None, scopes=None):
         if access_token is None:
@@ -38,68 +39,4 @@ class TestAuthValidator(TestCase):
         return Request("http://example.com/?client_id=%s" % (client_id,))
 
     def test_subclasses_request_validator(self):
-        validator = AuthValidator(self.auth)
-        self.assertTrue(isinstance(validator, RequestValidator))
-
-    @inlineCallbacks
-    def test_validate_bearer_token_fails_no_token(self):
-        validator = AuthValidator(self.auth)
-        request = self.mk_request("client1")
-        token = self.mk_token()
-        self.assertEqual(
-            (yield validator.validate_bearer_token(token, ["laser"], request)),
-            False)
-
-    @inlineCallbacks
-    def test_validate_bearer_token_fails_mismatched_access_token(self):
-        validator = AuthValidator(self.auth)
-        request = self.mk_request("client1")
-        token_good = self.mk_token()
-        token_bad = self.mk_token(access_token=u"bad horse")
-        yield self.auth.store_access_token("client1", token_good)
-        self.assertEqual(
-            (yield validator.validate_bearer_token(
-                token_bad, ["laser"], request)),
-            False)
-
-    @inlineCallbacks
-    def test_validate_bearer_token_fails_mismatched_scopes(self):
-        validator = AuthValidator(self.auth)
-        request = self.mk_request("client1")
-        token = self.mk_token(scopes=["laser", "periscope"])
-        yield self.auth.store_access_token("client1", token)
-        self.assertEqual(
-            (yield validator.validate_bearer_token(token, ["sith"], request)),
-            False)
-
-    @inlineCallbacks
-    def test_validate_bearer_token_fails_no_scopes(self):
-        validator = AuthValidator(self.auth)
-        request = self.mk_request("client1")
-        token = self.mk_token(scopes=["laser", "periscope"])
-        yield self.auth.store_access_token("client1", token)
-        self.assertEqual(
-            (yield validator.validate_bearer_token(token, [], request)),
-            False)
-
-    @inlineCallbacks
-    def test_validate_bearer_token_succeeds(self):
-        validator = AuthValidator(self.auth)
-        request = self.mk_request("client1")
-        token = self.mk_token(scopes=["laser", "periscope"])
-        yield self.auth.store_access_token("client1", token)
-        self.assertEqual(
-            (yield validator.validate_bearer_token(token, ["laser"], request)),
-            True)
-
-    @inlineCallbacks
-    def test_save_bearer_token(self):
-        validator = AuthValidator(self.auth)
-        request = self.mk_request("client1")
-        token = self.mk_token()
-        self.assertEqual(
-            (yield validator.save_bearer_token(token, request)),
-            None)
-        self.assertEqual(
-            (yield self.auth.retrieve_access_token("client1")),
-            token)
+        self.assertTrue(isinstance(self.validator, RequestValidator))
