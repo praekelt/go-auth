2a7e43dcb48d3cabd23e5928aaa04c724e5136ea
justinvdm <justinvdm.0.1.0@gmail.com>
2014-07-30 16:36:08 +0200

    Release 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="go-auth",
-    version="0.1.0a",
+    version="0.1.1",
     url='https://github.com/praekelt/go-auth',
     license='BSD',
     description="Authentication services and utilities for Vumi Go APIs.",
